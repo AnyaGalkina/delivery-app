40d7eb65f1cdfad5ea19e9370fa25467a8e1abea
Anna Galkina <anna.blackbird1@gmail.com>
2022-11-30 15:00:39 +0300

    Add Confirmation Page and email resend logic

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,29 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import thunk from 'redux-thunk';
 
+import { loadState, saveState } from '../common/utils/local-storage';
 import { authReducer } from '../features/auth';
 
 import { appReducer } from './app-reducer';
 
+export const rootReducer = combineReducers({
+  app: appReducer,
+  auth: authReducer,
+});
+
 export const store = configureStore({
-  reducer: {
-    app: appReducer,
-    auth: authReducer,
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk),
+  preloadedState: {
+    ...rootReducer,
+    // @ts-ignore
+    auth: { ...rootReducer.auth, accessToken: loadState() },
   },
+});
+
+store.subscribe(() => {
+  saveState(store.getState().auth.accessToken);
 });
 
 export type AppDispatch = typeof store.dispatch;
